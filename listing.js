5bcc440615e0f1a2f409dfe7a7511572c30cc442
Anup Mishra <anupm0873@gmail.com>
2024-05-23 23:58:06 +0530

    "add details"

---
+++ listing.js
@@ -0,0 +1,67 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+    } ,
+    description:String,
+    image: {
+        type:String,
+        default:"https://unsplash.com/photos/a-large-library-filled-with-lots-of-books-zb9Hg1Ln76s",
+        set:(v)=>
+            v===""
+        ?"https://unsplash.com/photos/a-large-library-filled-with-lots-of-books-zb9Hg1Ln76s"
+        :v,
+    },
+    price:String,
+    location:String,
+    country:String
+
+});
+
+
+const listing=mongoose.model("listing",listingSchema);
+module.exports=listing;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
